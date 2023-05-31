8c80d847d40092ad288f4ba3b5655b64de35ea80
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-05-31 20:02:41 +0530

    Time: 4 ms (68.22%) | Memory: 44.5 MB (5.91%) - LeetSync

---
+++ single-number-ii/single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        Arrays.sort(nums);
+        int i=0;
+       while(i<nums.length-1){
+           if( nums[i]==nums[i+1]){
+               i=i+3;
+           }
+           else{
+               return nums[i];
+           }
+       }
+       return nums[nums.length-1];
+    }
+}
