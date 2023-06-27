97cec1324b0108d13824d12a83f43d2220cf4b9e
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-06-27 18:07:27 +0530

    Time: 6 ms (44.90%) | Memory: 44.6 MB (37.28%) - LeetSync

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,25 +1,18 @@
 class Solution {
     public int singleNumber(int[] nums) {
-           if(nums.length==1){
+             if(nums.length==1){
             return nums[0];
         }
-      Arrays.sort(nums);
-      int required=nums[0];
-        int count=1;
+        Arrays.sort(nums);
         int i=0;
-      while(i<nums.length-1){
-        
-          if(nums[i]!=nums[i+1] && count==1){
-              return nums[i];
-          }
-          else{
-              i=i+2;
-              count++;
-              if(count==2){
-                  count=1;
-              }
-          }
-      }
-      return nums[nums.length-1];
+       while(i<nums.length-1){
+           if( nums[i]==nums[i+1]){
+               i=i+2;
+           }
+           else{
+               return nums[i];
+           }
+       }
+       return nums[nums.length-1];
     }
 }
