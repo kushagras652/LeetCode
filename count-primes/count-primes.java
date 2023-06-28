b96cae1e79a0266e11bad00e52b8b6ced97a4a42
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-06-28 19:44:20 +0530

    Time: 152 ms (36.27%) | Memory: 47.9 MB (57.04%) - LeetSync

---
+++ count-primes/count-primes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPrimes(int n) {
+       if(n<=2){
+           return 0;
+       }
+    //    if(n==2 || n==3){
+    //        return ;
+    //    }
+       boolean[] isComposite = new boolean[n];
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!isComposite[i]) {
+                count++;
+                for (int j = 2; i * j < n; j++) {
+                    isComposite[i * j] = true;
+                }
+            }
+        }
+
+        return count;
+    }
+}
