a4f36a64c21fbc3da2a547767f4199e71ef9f621
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-07-10 15:41:26 +0530

    Time: 12 ms (40.49%) | Memory: 62.6 MB (23.56%) - LeetSync

--- minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
+++ minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.java
@@ -15,20 +15,30 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        if(root==null){
+        // if(root==null){
+        //     return 0;
+        // }
+        // if(root.left==null && root.right==null){
+        //     return 1;
+        // }
+        // int left=Integer.MAX_VALUE;
+        // int right=Integer.MAX_VALUE;
+        // if(root.left!=null){
+        //  left=minDepth(root.left);
+        // }
+        // if(root.right!=null){
+        //  right=minDepth(root.right);
+        // }
+        // return Math.min(left, right)+1;
+            if (root == null)
             return 0;
-        }
-        if(root.left==null && root.right==null){
-            return 1;
-        }
-        int left=Integer.MAX_VALUE;
-        int right=Integer.MAX_VALUE;
-        if(root.left!=null){
-         left=minDepth(root.left);
-        }
-        if(root.right!=null){
-         right=minDepth(root.right);
-        }
-        return Math.min(left, right)+1;
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (root.left != null && root.right != null)
+            return Math.min(left, right) + 1;
+
+        return Math.max(left, right) + 1;
     }
 }
