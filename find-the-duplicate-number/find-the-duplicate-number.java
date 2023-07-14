66fd5d473e15c0a5a4f2708194635a1eaa39012f
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-07-14 20:58:24 +0530

    Time: 38 ms (21.90%) | Memory: 55.9 MB (97.61%) - LeetSync

--- find-the-duplicate-number/find-the-duplicate-number.java
+++ find-the-duplicate-number/find-the-duplicate-number.java
@@ -8,17 +8,26 @@ class Solution {
         //     if(nums[mid]==nums[mid+1] || nums[mid]==nums[mid-1]){
         //         return nums[mid];
         //     }
-
         // }
-        int i=0;
-        while(i<nums.length){
-             if(nums[i]!=nums[i+1]){
+    //     int i=0;
+    //     while(i<nums.length){
+    //          if(nums[i]!=nums[i+1]){
+    //         i++;
+    //     }
+    //     if(nums[i]==nums[i+1]){
+    //         return nums[i];
+    //     }
+    //     }
+    //    return -1;
+    int i=1;
+    while(i<nums.length){
+        if(nums[i]!=nums[i-1]){
             i++;
         }
-        if(nums[i]==nums[i+1]){
+        else{
             return nums[i];
         }
-        }
-       return -1;
+    }
+    return 0;
     }
 }
