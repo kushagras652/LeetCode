1ef3868843616625c34d00f9d2afe735b3123c63
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-07-31 18:56:12 +0530

    Time: 2 ms (19.52%) | Memory: 40.7 MB (43.54%) - LeetSync

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.equals(s2))
+        return true;
+        int count=0;
+        Set<Character> set1=new HashSet<>();
+        Set<Character> set2=new HashSet<>();
+
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                set1.add(s1.charAt(i));
+                set2.add(s2.charAt(i));
+                count++;
+            }
+        }
+        if(count==2 && set1.equals(set2))
+        return true;
+        return false;
+    }
+}
