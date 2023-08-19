e118b25d7b19c003754c47ac099f9e71f10d635b
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-08-19 10:31:11 +0530

    Time: 9 ms (32.48%) | Memory: 62.7 MB (6.01%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,19 +1,29 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        PriorityQueue<Integer> p=new PriorityQueue<>();
+        // PriorityQueue<Integer> p=new PriorityQueue<>();
+        // for(Integer a:nums){
+        //     if(a>0)
+        //     p.add(a);
+        // }
+        //  int a=1;
+        // while(!p.isEmpty()){
+        //     int b=p.poll();
+        //     if(b==a){
+        //         a++;
+        //     }
+        //     else if(b>a){
+        //         return a;
+        //     }
+        // }
+        // return a;
+        HashMap<Integer,Boolean> hp=new HashMap<>();
         for(Integer a:nums){
             if(a>0)
-            p.add(a);
+            hp.put(a,true);
         }
-         int a=1;
-        while(!p.isEmpty()){
-            int b=p.poll();
-            if(b==a){
-                a++;
-            }
-            else if(b>a){
-                return a;
-            }
+        int a=1;
+        while(hp.containsKey(a)){
+            a++;
         }
         return a;
     }
