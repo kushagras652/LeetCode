80b4ae538d480b2b20a2bfb2340b0dbe4ab89049
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-08-24 21:49:44 +0530

    Time: 8 ms (7.71%) | Memory: 39.8 MB (57.30%) - LeetSync

---
+++ 1660-thousand-separator/thousand-separator.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String thousandSeparator(int n) {
+        if(n>=1000){
+            String str=Integer.toString(n);
+            int len=str.length();
+            int pos=len-3;
+            while(pos>=1){
+                str=str.substring(0,pos)+'.'+str.substring(pos);
+                pos=pos-3;
+            }
+            return str;
+        }
+        else
+            return Integer.toString(n);
+    }
+}
