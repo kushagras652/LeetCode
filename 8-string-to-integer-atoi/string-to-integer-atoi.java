37e19e5b2ea42d7c8f1ef00d34a80a6a92ee1a9f
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-09-18 11:52:06 +0530

    Time: 1 ms (100.00%) | Memory: 40.9 MB (95.90%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,33 +1,38 @@
 class Solution {
     public int myAtoi(String s) {
-        long num = 0;
-        char[] chars = s.toCharArray();
+        long num=0;
+        char[] chars=s.toCharArray();
 
-        if (chars.length == 0) return 0;
-        int i = 0, sign = 1; 
-        char current = chars[i];
+        if(chars.length==0)return 0;
+        int i=0,sign=1;
+        char current=chars[i];
 
-        while (current == ' ') {
+        while(current==' '){
             i++;
-            if (i == chars.length) return 0;
-            current = chars[i];
+            if(i==chars.length)return 0;
+            current=chars[i];
         }
-        if (current == '-') {
-            sign = -1;
-            i++;
-        } else if (current == '+') {
+        if(current=='-'){
+            sign=-1;
             i++;
         }
-        for (; i < chars.length; i++) {
-            current = chars[i];
-            if (current < '0' || current > '9') break;
-
-            num *= 10;
-            num += current - '0';
-
-            if (sign * num <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
-            else if (sign * num >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        else if(current=='+'){
+            sign=1;
+            i++;
         }
-        return (int) (sign * num);
+        for(;i<chars.length;i++){
+            current=chars[i];
+            if(current<'0' || current>'9')break;
+
+            else{
+                num*=10;
+                num+=current-'0';
+
+                if(sign*num<=Integer.MIN_VALUE)return Integer.MIN_VALUE;
+                else if(sign*num>=Integer.MAX_VALUE)return Integer.MAX_VALUE;
+            }
+        }
+        num*=sign;
+        return (int)num;
     }
 }
