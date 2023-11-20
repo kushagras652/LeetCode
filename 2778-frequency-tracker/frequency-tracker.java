f281b2bbf3d45ca3042f93dca5a9ca87b46ce4dd
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-11-20 21:36:33 +0530

    Time: 40 ms (59.09%) | Memory: 113.5 MB (24.24%) - LeetSync

---
+++ 2778-frequency-tracker/frequency-tracker.java
@@ -0,0 +1,35 @@
+class FrequencyTracker {
+    int[] freq;
+    int[] count;
+
+    public FrequencyTracker() {
+        freq=new int[100001];
+        count=new int[100001];
+    }
+    
+    public void add(int number) {
+        freq[count[number]]--;
+        count[number]++;
+        freq[count[number]]++;
+    }
+    
+    public void deleteOne(int number) {
+        if(count[number]>0){
+            freq[count[number]]--;
+            count[number]--;
+            freq[count[number]]++;
+        }
+    }
+    
+    public boolean hasFrequency(int frequency) {
+        return freq[frequency]>=1;
+    }
+}
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker obj = new FrequencyTracker();
+ * obj.add(number);
+ * obj.deleteOne(number);
+ * boolean param_3 = obj.hasFrequency(frequency);
+ */
