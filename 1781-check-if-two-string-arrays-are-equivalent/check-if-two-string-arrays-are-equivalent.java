310bf923dcc45bdecc0edbedaa8bd0d3f50042c8
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-12-01 14:43:25 +0530

    Time: 2 ms (12.08%) | Memory: 40.5 MB (43.24%) - LeetSync

---
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String st1="";
+        String st2="";
+        for(int i=0;i<word1.length;i++){
+            st1+=word1[i];
+        }
+        for(int i=0;i<word2.length;i++){
+            st2+=word2[i];
+        }
+        if(st1.length()==st2.length()){
+            for(int i=0;i<st1.length();i++){
+                if(st1.charAt(i)!=st2.charAt(i))
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
