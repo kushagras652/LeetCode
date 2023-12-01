70d191ea045faf3bfe4cc95c95710f1cbeda7a8a
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2023-12-01 14:44:51 +0530

    Time: 1 ms (73.30%) | Memory: 40.8 MB (15.80%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -8,13 +8,15 @@ class Solution {
         for(int i=0;i<word2.length;i++){
             st2+=word2[i];
         }
-        if(st1.length()==st2.length()){
-            for(int i=0;i<st1.length();i++){
-                if(st1.charAt(i)!=st2.charAt(i))
-                return false;
-            }
-            return true;
-        }
+        // if(st1.length()==st2.length()){
+        //     for(int i=0;i<st1.length();i++){
+        //         if(st1.charAt(i)!=st2.charAt(i))
+        //         return false;
+        //     }
+        //     return true;
+        // }
+        if(st1.equals(st2))
+        return true;
         return false;
     }
 }
