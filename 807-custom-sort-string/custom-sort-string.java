a3d6df2aba9ac89253d370750a82d8747614ff22
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-03-11 23:36:41 +0530

    Time: 2 ms (62.70%) | Memory: 41.4 MB (62.59%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -3,7 +3,6 @@ class Solution {
 
        HashMap<Character, Integer> charCount = new HashMap<>();
 
-        // Count the occurrences of each character in s
         for (int i = 0; i < s.length(); i++) {
             char currentChar = s.charAt(i);
             charCount.put(currentChar, charCount.getOrDefault(currentChar, 0) + 1);
@@ -11,12 +10,11 @@ class Solution {
 
         StringBuilder st = new StringBuilder();
 
-        // Iterate over the characters in the order
         for (int i = 0; i < order.length(); i++) {
             char currentChar = order.charAt(i);
             if (charCount.containsKey(currentChar)) {
                 int count = charCount.get(currentChar);
-                // Append the character count times
+
                 for (int j = 0; j < count; j++) {
                     st.append(currentChar);
                 }
@@ -24,7 +22,6 @@ class Solution {
             }
         }
 
-        // Append the remaining characters
         for (char remaining : charCount.keySet()) {
             int count = charCount.get(remaining);
             for (int i = 0; i < count; i++) {
