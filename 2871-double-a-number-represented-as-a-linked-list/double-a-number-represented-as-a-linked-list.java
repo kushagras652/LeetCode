7ce1f26ac00b2a0befe2d28219692ea54d0253b3
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-05-07 09:50:24 +0530

    Time: 2 ms (100.00%) | Memory: 46.6 MB (41.01%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.java
@@ -10,7 +10,42 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        if (head.val > 4)
+
+        //BELOW CODE IS CORRECT BUT IS NOT WORKING WHEN VALUE OF NUMBER 
+        //BECOMES BIGGER THAN LONG DATATYPE
+
+
+        // long num=0;
+        // ListNode temp=head;
+        // while(temp!=null){
+        //     num=num*10+temp.val;
+        //     temp=temp.next;
+        // }
+        // num=num*2;
+
+        //   ListNode result = new ListNode((int)(num % 10));
+        // num /= 10;
+        // temp = result;
+        // while (num > 0) {
+        //     temp.next = new ListNode((int)(num % 10));
+        //     num /= 10;
+        //     temp = temp.next;
+        // }
+        
+        // ListNode prev = null;
+        // ListNode current = result;
+        // ListNode nextNode = null;
+        
+        // while (current != null) {
+        //     nextNode = current.next;
+        //     current.next = prev;
+        //     prev = current;
+        //     current = nextNode;
+        // }
+        
+        // return prev;
+
+         if (head.val > 4)
             head = new ListNode(0, head);
         for(ListNode node = head; node != null; node = node.next) {
             node.val = (node.val * 2) % 10;
@@ -18,5 +53,7 @@ class Solution {
                 node.val++;
         }
         return head;
+
+
     }
-    }
+}
