984cc99769091a9cde5ff54b9b0bc0c5cc5262a4
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-05-17 09:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (69.47%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.java
@@ -15,18 +15,20 @@
  */
 class Solution {
     public boolean evaluateTree(TreeNode root) {
-        return helper(root);
-    }
-    public boolean helper(TreeNode root){
-        if(root.val==0 || root.val==1)
+       // return helper(root);
+
+         if(root.val==0 || root.val==1)
         return root.val==1;
         else if(root.val==2){
-            return helper(root.left) || helper(root.right);
+            return evaluateTree(root.left) || evaluateTree(root.right);
         }
         else if(root.val==3){
-            return helper(root.left) && helper(root.right);
+            return evaluateTree(root.left) && evaluateTree(root.right);
         }
 
         return false;
     }
+    // public boolean helper(TreeNode root){
+       
+    // }
 }
