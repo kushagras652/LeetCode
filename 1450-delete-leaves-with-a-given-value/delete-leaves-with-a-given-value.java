8a4ce0194eca7ea90ac140adad61472ae57e6ec7
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-05-17 09:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (23.89%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.java
@@ -18,12 +18,12 @@ class Solution {
         if(root==null)
         return null;
 
-          root.left=removeLeafNodes(root.left,target);
-            root.right=removeLeafNodes(root.right,target);
+        root.left=removeLeafNodes(root.left,target);
+        root.right=removeLeafNodes(root.right,target);
 
-            if(root.left==null && root.right==null && root.val==target)
-            return null;
-     
+        if(root.left==null && root.right==null && root.val==target)
+        return null;
+
         return root;
     }
 }
