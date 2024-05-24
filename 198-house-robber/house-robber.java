c30e1d03cf7d50246f0d0782d5ae03ac65c3431c
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-05-25 00:51:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (6.58%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,12 +1,37 @@
 class Solution {
     public int rob(int[] arr) {
-       if(arr.length==0)return 0;
-        int dp[] = new int[arr.length+1];
-        dp[0]=0;
-        dp[1]=arr[0];
-        for(int i=1; i<arr.length; i++){
-            dp[i+1]=Math.max(dp[i],dp[i-1]+arr[i]);
-        }
-        return dp[arr.length];
+
+    //    if(arr.length==0)return 0;
+    //     int dp[] = new int[arr.length+1];
+    //     dp[0]=0;
+    //     dp[1]=arr[0];
+    //     for(int i=1; i<arr.length; i++){
+    //         dp[i+1]=Math.max(dp[i],dp[i-1]+arr[i]);
+    //     }
+    //     return dp[arr.length];
+
+        int n=arr.length;
+        int[] dp=new int[n+1];
+        Arrays.fill(dp,-1);
+    int ans=solve(dp,n-1,arr);
+    return ans;
+    }
+
+    int solve(int[] dp,int n,int[] arr){
+        if(n==0)
+        return arr[0];
+
+        if(n<0)
+        return 0;
+
+        if(dp[n]!=-1)
+        return dp[n];
+
+        int in=arr[n]+solve(dp,n-2,arr);
+        int ex=solve(dp,n-1,arr);
+
+        dp[n]=Math.max(in,ex);
+
+        return dp[n];
     }
 }
