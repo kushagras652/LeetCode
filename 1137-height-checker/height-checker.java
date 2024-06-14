f44f436d750a4b5db747c430c2be6c72699b991f
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-06-15 02:21:36 +0530

    Time: 2 ms (85.33%) | Memory: 41.4 MB (18.86%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        // boolean swapped;
+        // int[] expected=new int[heights.length];
+        // System.arraycopy(heights, 0,expected, 0,heights.length);
+        // for(int i=0;i<expected.length;i++){
+        //     swapped=false;
+        //     for(int j=0;j<expected.length-i-1;j++){
+        //         if(expected[j]>expected[j+1]){
+        //             int temp=expected[j];
+        //            expected[j]=expected[j+1];
+        //             expected[j+1]=temp;
+        //             swapped=true;
+        //         }
+        //     }
+        //     if(!swapped)
+        //     break;
+        // }
+        // int count=0;
+        // for(int i=0;i<heights.length;i++){
+        //     if(heights[i]!=expected[i]){
+        //         count++;
+        //     }
+        // }
+        // return count;
+
+
+        int count=0;
+        int[] expected=new int[heights.length];
+         System.arraycopy(heights, 0,expected, 0,heights.length);
+         Arrays.sort(expected);
+         for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i])
+            count++;
+         }
+         return count;
+    }
+}
