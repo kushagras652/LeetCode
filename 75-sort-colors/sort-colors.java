9dae0171f55b5f781e8f946415fc478ecd0277ad
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-07-18 16:54:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (56.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -8,25 +8,47 @@ class Solution {
         //     nums[i]=pq.poll();
         // }
 
-        int count0=0;
-        int count1=0;
-        int count2=0;
+        //BIT OPTIMIZED 
+        // int count0=0;
+        // int count1=0;
+        // int count2=0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0)
-            count0++;
-            else if(nums[i]==1)
-            count1++;
-            else
-            count2++;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0)
+        //     count0++;
+        //     else if(nums[i]==1)
+        //     count1++;
+        //     else
+        //     count2++;
+        // }
+
+        // for(int i=0;i<count0;i++)
+        // nums[i]=0;
+        // for(int i=count0;i<count0+count1;i++)
+        // nums[i]=1;
+        // for(int i=count0+count1;i<nums.length;i++)
+        // nums[i]=2;
+
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
         }
-
-        for(int i=0;i<count0;i++)
-        nums[i]=0;
-        for(int i=count0;i<count0+count1;i++)
-        nums[i]=1;
-        for(int i=count0+count1;i<nums.length;i++)
-        nums[i]=2;
-      
     }
 }
