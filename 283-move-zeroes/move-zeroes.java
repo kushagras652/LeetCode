14140e78a99f6800de376c8e07ad7c639f6e2fcd
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-07-18 14:32:03 +0530

    Time: 2 ms (82.76%) | Memory: 46.5 MB (8.92%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,27 +1,39 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        // Arrays.sort(nums);
+
+        //BRUTE FORCE
+        // int[] arr=new int[nums.length];
+        // int j=0;
         // for(int i=0;i<nums.length;i++){
-        //      if(nums[i]!=0)
-        //     break;
-        //     if(nums[i]==0){
-        //         int temp=nums[i];
-        //         nums[i]=nums[nums.length-i-1];
-        //         nums[nums.length-i-1]=temp;
+        //     if(nums[i]!=0){
+        //         arr[j++]=nums[i];
         //     }
-           
         // }
 
-        int[] arr=new int[nums.length];
-        int j=0;
+        // for(int i=0;i<nums.length;i++){
+        //     nums[i]=arr[i];
+        // }
+
+        int j=-1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                arr[j++]=nums[i];
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
+        if(j==-1)
+        return ;
 
-        for(int i=0;i<nums.length;i++){
-            nums[i]=arr[i];
+        for(int i=j+1;i<nums.length;i++){
+
+            if(nums[i]!=0){
+                int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+
+            j++;
+            }
+            
         }
 
     }
