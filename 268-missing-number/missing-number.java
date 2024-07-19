f2adbabdf8955b6c84f13feb0066fcccb87ea303
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-07-19 11:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (87.04%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -10,12 +10,23 @@ class Solution {
         // }
         // return nums.length;
 
-        int n=nums.length;
-        int sum=(n*(n+1))/2;
-        int sum2=0;
-        for(int elem:nums){
-            sum2+=elem;
+
+        //BETTER APPROACH
+        // int n=nums.length;
+        // int sum=(n*(n+1))/2;
+        // int sum2=0;
+        // for(int elem:nums){
+        //     sum2+=elem;
+        // }
+        // return sum-sum2;
+
+        //OPTIMAL APPROACH
+        int xor1=0,xor2=0;
+        for(int i=0;i<nums.length;i++){
+            xor1=xor1^nums[i];
+            xor2=xor2^(i+1);
         }
-        return sum-sum2;
+      //  xor2=xor2^nums.length;
+        return xor1^xor2;
     }
 }
