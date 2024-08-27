8df783a92b280afdadc2227d422af2b4410aa6fa
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-08-27 11:44:58 +0530

    Time: 1 ms (54.82%) | Memory: 44.6 MB (90.78%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,24 +1,36 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        List<Integer> list1=new ArrayList<>();
-        List<Integer> list2=new ArrayList<>();
-        for(int elem:nums){
-            if(elem%2==0){
-                list1.add(elem);
+        // List<Integer> list1=new ArrayList<>();
+        // List<Integer> list2=new ArrayList<>();
+        // for(int elem:nums){
+        //     if(elem%2==0){
+        //         list1.add(elem);
+        //     }
+        //     else{
+        //         list2.add(elem);
+        //     }
+        // }
+        //     int i=0;
+        // while(!list1.isEmpty()){
+        //     nums[i]=list1.remove(0);
+        //     i++;
+        // }
+        //   while(!list2.isEmpty()){
+        //     nums[i]=list2.remove(0);
+        //     i++;
+        // }
+        // return nums;
+
+        int start=0,end=nums.length-1;
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                arr[start++]=nums[i];
             }
             else{
-                list2.add(elem);
+                arr[end--]=nums[i];
             }
         }
-            int i=0;
-        while(!list1.isEmpty()){
-            nums[i]=list1.remove(0);
-            i++;
-        }
-          while(!list2.isEmpty()){
-            nums[i]=list2.remove(0);
-            i++;
-        }
-        return nums;
+        return arr;
     }
 }
