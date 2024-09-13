de9b85ee9e827443ad9a623e3e24beacb2c3b4de
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-09-13 23:40:11 +0530

    Time: 3 ms (49.01%) | Memory: 45.6 MB (58.41%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max=0;
+        // if(nums.length==1)
+        // return true;
+        for(int i=0;i<nums.length;i++){
+
+             if(i>max)
+             return false;
+
+             max=Math.max(max,i+nums[i]);
+        }
+        return true;
+    }
+}
