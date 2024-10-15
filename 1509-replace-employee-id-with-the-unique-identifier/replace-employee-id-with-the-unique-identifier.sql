1cfdbd7e8dc2c735ed1996b58465cb3f2c89ced9
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-10-15 12:17:02 +0530

    Time: 1025 ms (76.46%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-#SELECT uni.unique_id,emp.name
-#FROM Employees as emp
-#LEFT JOIN EmployeeUNI as uni
-#ON uni.id=emp.id;
-
-SELECT uni.unique_id,emp.name
-FROM EmployeeUNI as uni
-RIGHT JOIN Employees as emp
-ON uni.id=emp.id;
+select unique_id,name
+from Employees e
+left join EmployeeUNI eu
+on e.id=eu.id;
