720b1ed86d0c2e7b233ca61218485c81dcadcc10
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2024-10-15 12:29:57 +0530

    Time: 337 ms (87.40%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select w1.id
-from weather w1
-left join weather w2
-on w1.recordDate -INTERVAL 1 DAY=w2.recordDate
+from Weather w1
+left join Weather w2
+on w1.recordDate-INTERVAL 1 DAY=w2.recordDate
 where w1.temperature>w2.temperature;
