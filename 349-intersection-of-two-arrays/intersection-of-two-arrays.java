a3c66a793cb6d74f0489d92c4345fc1b99a0faf6
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2025-01-11 12:51:20 +0530

    Time: 3 ms (55.06%) | Memory: 43.7 MB (12.73%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,39 +1,18 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-       ArrayList<Integer> list=new ArrayList<>();
-       for(int num:nums1){
-           if(binarySearch(nums2,num)){
-               list.add(num);
-            //    if(list.contains(num)){
-            //        list.remove(num);
-            //    }
-           }
-       }
-        HashSet<Integer> uniqueSet = new HashSet<>(list);
-        ArrayList<Integer> list2=new ArrayList<>(uniqueSet);
-       int[] intersectionArray=new int[list2.size()];
-       for(int i=0;i<list2.size();i++){
-           intersectionArray[i]=list2.get(i);
-       }
-       return intersectionArray;
-    }
-    public boolean binarySearch(int[] arr,int target){
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int middle=start+(end-start)/2;
-            if(target<arr[middle]){
-                end=middle-1;
-            }
-            else if(target>arr[middle]){
-                start=middle+1;
-            }
-            else{
-                return true;
+        Set<Integer> set=new HashSet<>();
+
+        for(int elem:nums1){
+            set.add(elem);
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int elem:nums2){
+            if(set.contains(elem)){
+                list.add(elem);
+                set.remove(elem);
             }
         }
-        return false;
+        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+        return array;
     }
 }
