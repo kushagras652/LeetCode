7ee6b358cf0eb32a782371e949b5640873493957
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2025-04-07 14:12:26 +0530

    Time: 11 ms (43.16%) | Memory: 47.4 MB (93.14%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        //this shows error if there are negetive integrs in array 
+        //bcoz array canot store  for negative values 
+        // List<Integer> list=new ArrayList<>();
+        // int n=nums.length;
+        // int a=n/3;
+        // Arrays.sort(nums);
+        // int[] freq=new int[nums[n-1]+1];
+        // for(int i=0;i<n;i++){
+        //     freq[nums[i]]++;
+        // }
+        // for(int i=0;i<freq.length;i++){
+        //     if(freq[i]>a)
+        //     list.add(i);
+        // }
+        // return list;
+
+         List<Integer> list=new ArrayList<>();
         int n=nums.length;
-        Arrays.sort(nums);
-        int count=1;
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==nums[i+1])
-            count++;
+        int a=n/3;
 
-            if(nums[i]!=nums[i+1])
-            count=1;
-
-            if(count>n/3){
-                if(!list.contains(nums[i]))
-                list.add(nums[i]);
-            }
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int elem:nums){
+            map.put(elem,map.getOrDefault(elem,0)+1);
         }
-         if (count > n/3 && !list.contains(nums[nums.length - 1])) {
-            list.add(nums[nums.length - 1]);
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            if(entry.getValue()>a){
+                list.add(entry.getKey());
+            }
         }
         return list;
     }
