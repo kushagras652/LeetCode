6d35fa3b27ffe9864de4cd3eefd937624f3e464a
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2026-04-09 22:24:49 +0530

    Time: 31 ms (82.61%) | Memory: 60.5 MB (6.50%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,59 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-       Set<List<Integer>> ds=new HashSet<>();
-       Arrays.sort(nums);
-       int target=0;
-       for(int i=0;i<nums.length;i++){
-          int j=i+1;
-          int k=nums.length-1;
-          while(j<k){
-              int sum=nums[i]+nums[j]+nums[k];
-              if(sum==target){
-                  ds.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                  j++;
-                  k--;
-              }
-              else if(sum<target){
-                  j++;
-              }
-              else{
-                  k--;
-              }
-          }
-       }
-       result.addAll(ds);
-       return result;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        Arrays.sort(nums);
+        // HashSet<Integer> set=new HashSet<>();
+        List<List<Integer>> list2=new ArrayList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         int sum=nums[i]+nums[j];
+        //         if(set.contains(-sum)){
+        //             ArrayList<Integer> list=new ArrayList<>();
+        //             list.add(nums[i]);
+        //             list.add(nums[j]);
+        //             list.add(-sum);
+        //             if(!list2.contains(list))
+        //             list2.add(list);
+        //         }
+                
+        //         set.add(nums[j]);
+        //     }
+        //     set.add(nums[i]);
+        // }
+
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+            int left=i+1,right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+            if(sum<0){
+                left++;
+            }
+            else if(sum>0){
+                right--;
+            }else{
+                ArrayList<Integer> list=new ArrayList<>();
+                list.add(nums[i]);
+                list.add(nums[left]);
+                list.add(nums[right]);
+                list2.add(list);
+                left++;
+                right--;
+
+                while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // skip duplicate right
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+            }
+            }
+            
+        }
+        
+        return list2;
     }
 }
