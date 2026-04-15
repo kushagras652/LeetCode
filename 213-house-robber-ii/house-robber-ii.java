2ee61b6e7d05c7f271529c66a0e863c56ce3f930
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2026-04-15 10:15:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (27.20%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -3,31 +3,22 @@ class Solution {
         int n=nums.length;
         if(n==1)
         return nums[0];
-        
-        int[] arr1=Arrays.copyOfRange(nums,0,n-1);
-        int[] arr2=Arrays.copyOfRange(nums,1,n);
 
-        return Math.max(helper(arr1),helper(arr2));
+        int case1=solve(nums,0,n-2);
+        int case2=solve(nums,1,n-1);
+
+        return Math.max(case1,case2);
     }
 
-    int helper(int[] nums){
-        int n=nums.length;
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return func(n-1,nums,dp);
-    }
-    int func(int n,int[] nums,int[] dp){
-        if(n==0)
-        return nums[0];
-        if(n<0)
-        return 0;
+    int solve(int[] nums,int start,int end){
+        int prev1=0;
+        int prev2=0;
 
-        if(dp[n]!=-1)
-        return dp[n];
-
-        int take=nums[n]+func(n-2,nums,dp);
-        int not=func(n-1,nums,dp);
-
-        return dp[n]=Math.max(take,not);
+        for(int i=start;i<=end;i++){
+            int curr=Math.max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
     }
 }
