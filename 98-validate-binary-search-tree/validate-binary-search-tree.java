8ae2265df43d37dcb04378308807884c303b6de5
Kushagra Sharma <110619668+kushagras652@users.noreply.github.com>
2026-05-13 20:31:30 +0530

    Time: 2 ms (12.72%) | Memory: 45.2 MB (32.95%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,20 +14,26 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> list=new ArrayList<>();
     public boolean isValidBST(TreeNode root) {
-        inorder(root);
+        ArrayList<Integer> list=new ArrayList<>();
+        inorder(root,list);
+
         for(int i=0;i<list.size()-1;i++){
             if(list.get(i)>=list.get(i+1))
             return false;
         }
         return true;
     }
-    public void inorder(TreeNode root){
-        if(root==null)
-        return;
-        inorder(root.left);
+
+    public void inorder(TreeNode root,ArrayList<Integer> list){
+        if(root==null){
+            return ;
+        }
+
+        inorder(root.left,list);
         list.add(root.val);
-        inorder(root.right);
+        inorder(root.right,list);
+
+        // return list;
     }
 }
